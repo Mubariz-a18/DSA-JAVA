f7bc4e54e2f1c99192cce074d1e1b0d6a17286ba
mubariz ahmed <syedmubariz542@gmail.com>
2023-09-09 21:47:01 +0530

    Introduction to java and core concepts (variables,print,operators)

---
+++ Main.java
@@ -0,0 +1,98 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // how to print output
+        // System.out.println("hello world");
+
+        // Types of Primitive Data types
+        /*
+         * Numerical
+         * Int
+         * Short
+         * Double
+         * Float
+         * Long
+         * String
+         * char
+         * String
+         */
+
+        // int a = 10;
+        // int b = 20;
+        // System.out.println(a+b);
+
+        // short a = 255;
+        // short b = 1;
+        // System.out.println(a+b);
+
+        // double a = 1.25;
+        // double b = 0.75;
+        // System.out.println(a+b);
+
+        // long a = 10000000;
+        // long b = 100000000;
+        // System.out.println(a+b);
+
+        // char a = 'a';
+        // System.out.println(a);
+
+        // String name = "John \n" +
+        //         " is \n" +
+        //         "in engineering\n";
+
+        // System.out.println(name);
+
+
+        /**
+         * Arthematic Operators
+         * + , - , * , / , %
+         */
+
+        //  int a = 30;
+        //  int b = 20;
+        //  System.out.println(a+b);
+        //  System.out.println(a-b);
+        //  System.out.println(a/b);
+        //  System.out.println(a*b);
+        //  System.out.println(a%b);
+
+
+        /**
+         * Bitwise operator
+         * left shift
+         * right shift
+         * bitwise and 
+         * bitwise or
+         * x or 
+         */
+
+        //  int a = 3;
+        //  System.out.println(a>>1); // right
+        //  System.out.println(a<<1); // left
+        //  System.out.println(a&1);  //AND
+        //  System.out.println(a|1);  // OR
+    }
+}
+
+/* 
+d to b 13
+Decimal    Quotient/2    Remainder (Binary Digit)
+13         13          1
+6          6           0
+3          3           1
+1          1           1
+0 (Quotient is now 0, stop)
+
+
+
+b to d 1048
+Binary   Position   Calculation   Result
+1        2^0        1 * 2^0       1
+0        2^1        0 * 2^1       0
+1        2^2        1 * 2^2       4
+1        2^3        1 * 2^3       8
+
+
+
+
+ */
