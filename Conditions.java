a63b56b14208ecca9a7c8078ce5e75a97e7c49a1
mubariz ahmed <syedmubariz542@gmail.com>
2023-09-11 00:21:53 +0530

    conditions in java if else  elseif switch

---
+++ Conditions.java
@@ -0,0 +1,51 @@
+public class Conditions {
+    public static void main(String[] args) {
+
+        // if else blocks
+
+        // int a = 10;
+        // int b = 20;
+        // if(a>b){
+        // System.out.println("a is greater than b");
+        // }else{
+        // System.out.println("b is greater than a");
+        // }
+
+        // if else if else
+        // int aggregate = 50;
+        // if (aggregate >= 70) {
+        //     System.out.println("Distiction");
+        // } else if (aggregate >= 60) {
+        //     System.out.println("1st class");
+        // } else if (aggregate >= 40) {
+        //     System.out.println("2st class");
+        // } else if (aggregate >= 30) {
+        //     System.out.println("just pass");
+        // } else {
+        //     System.out.println("failed");
+        // }
+
+        //switch case only eq check
+        // int i = 70;
+        // switch(i){
+        //     case  70:
+        //         System.out.println("dist");
+        //         break;
+        //     case  50:
+        //         System.out.println("1");
+        //         break;
+                
+        //     case  40:
+        //         System.out.println("2");
+        //         break;
+        //     case  30:
+        //         System.out.println("3");
+        //         break;
+        //     default:
+        //         System.out.println("fail");
+            
+        // }
+
+        
+    }
+}
