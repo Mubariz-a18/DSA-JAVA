dc83973560c866562dd7d3f966084d312b680ff8
mubariz ahmed <syedmubariz542@gmail.com>
2023-09-12 00:11:21 +0530

    methods (function) method overloading function calling

---
+++ Methods.java
@@ -0,0 +1,25 @@
+// public class Methods {
+//     public static void main(String[] args) {
+        // int added = add(10,20);
+        // System.out.println(added);
+
+        // float added = add(10.2f,20.2f);
+        // System.out.println(added);
+
+//         int added = add(10,20,80);
+//         System.out.println(added);
+//     }
+
+//     static int add(int a ,int b){
+//         return a + b;
+//     }
+
+//     static float add(float a, float b){
+//         return a + b;
+//     }
+
+//         static int add(int a, int b, int c){
+//         return a + b + c;
+//     }
+// }
+
