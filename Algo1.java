16f8babb474a7c56658053659ea45314bd4232ac
mubariz ahmed <syedmubariz542@gmail.com>
2023-09-12 00:33:58 +0530

    basic algorithm

---
+++ Algo1.java
@@ -0,0 +1,45 @@
+public class Algo1 {
+    public static void main(String[] args) {
+
+
+        // boolean val = isPrime(110);
+        // System.out.println(val);
+
+    //    isEvevOrOdd(0);
+    
+    }
+
+    // find out if a number is prime or not
+/*
+ 
+ static boolean isPrime( int n ){
+     if(n == 1){
+         return false;
+        } else{
+            for(int i = 2;i<= (int)Math.sqrt(n);i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+*/
+
+
+
+/*
+
+// if n is even or odd
+    static void isEvevOrOdd(int n ){
+        if(n == 1){
+            System.out.println("not evn noy odd");  
+        }else if( n % 2==0){
+            System.out.println("even");
+        }else if(n % 2 == 1){
+            System.out.println("odd");
+        }
+    }
+    */
+}
+        
